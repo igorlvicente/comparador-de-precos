c1012c62a86c1ea3371836d70f39f682f9c3ff46
Igor L. Vicente <igorlemosvicente@gmail.com>
2020-09-19 11:57:11 -0300

    🔨 altera atom Keypad

--- src/components/atoms/Keypad.tsx
+++ src/components/atoms/Keypad.tsx
@@ -1,25 +1,26 @@
 import * as React from 'react'
 import { Button, Col, Row } from 'antd'
-import { CaretLeftOutlined, CheckOutlined } from '@ant-design/icons'
+import { CaretLeftOutlined, CheckOutlined, CloseOutlined } from '@ant-design/icons'
 
 type CalculatorContentProps = {
     onPress: { (new_value: string): void }
 }
 
-const keys: { value: string, label?: JSX.Element | string, type?: 'primary' | undefined, danger?: boolean, colSize?: number }[] = [
+const keys: { value: string, label?: JSX.Element | string, type?: 'primary' | 'dashed' | undefined, danger?: boolean, colSize?: number }[] = [
     { value: '1', label: '1' },
     { value: '2', label: '2' },
     { value: '3', label: '3' },
+    { value: 'X', label: <CloseOutlined />, type: 'dashed' },
     { value: '4', label: '4' },
     { value: '5', label: '5' },
     { value: '6', label: '6' },
+    { value: '<', label: <CaretLeftOutlined/>, danger: true, type: 'primary' },
     { value: '7', label: '7' },
     { value: '8', label: '8' },
     { value: '9', label: '9' },
     { value: ',', label: ',' },
-    { value: '0', label: '0' },
-    { value: '<', label: <CaretLeftOutlined/>, danger: true, type: 'primary' },
-    { value: 'OK', type: 'primary', label: <CheckOutlined/>, colSize: 24 },
+    { value: '0', label: '0', colSize: 18 },
+    { value: 'OK', type: 'primary', label: <CheckOutlined/> },
 ]
 
 const Keypad: React.FC<CalculatorContentProps> = (props) => {
@@ -32,12 +33,12 @@ const Keypad: React.FC<CalculatorContentProps> = (props) => {
             {
                 keys.map(key => (
                     <Col
-                        xs={key.colSize || 8}
-                        sm={key.colSize || 8}
-                        md={key.colSize || 8}
-                        lg={key.colSize || 8}
-                        xl={key.colSize || 8}
-                        xxl={key.colSize || 8}
+                        xs={key.colSize || 6}
+                        sm={key.colSize || 6}
+                        md={key.colSize || 6}
+                        lg={key.colSize || 6}
+                        xl={key.colSize || 6}
+                        xxl={key.colSize || 6}
                     >
                         <Button
                             block
